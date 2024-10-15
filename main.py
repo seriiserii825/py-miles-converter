b3a1925ba545c2ea14eeb75cc4dc58d7e45a97d0
serii burduja <seriiburduja@gmail.com>
2024-10-15 18:31:54 +0300

    feat: finished pomodoro

--- main.py
+++ main.py
@@ -6,28 +6,60 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 1
+SHORT_BREAK_MIN = 2
+LONG_BREAK_MIN = 3
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def reset_timer():
+    global reps
+    if timer:
+        window.after_cancel(timer)
+    reps = 0
+    canvas.itemconfig(timer_text, text="00:00")
+    label_title.config(text="Timer", fg=GREEN)
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    reps += 1
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        label_title.config(text="Long Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        label_title.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        label_title.config(text="Work", fg=GREEN)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
+    global reps
+    global timer
     minutes = count // 60
     if minutes < 10:
         minutes = f"0{minutes}"
     seconds = count % 60
     if seconds < 10:
         seconds = f"0{seconds}"
+
     canvas.itemconfig(timer_text, text=f"{minutes}:{seconds}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        timer = window.after(50, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = reps // 2
+        for _ in range(work_sessions):
+            marks += "✔"
+        label_check.config(text=marks)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -46,10 +78,10 @@ canvas.grid(column=1, row=1)
 button_start = Button(text="Start", highlightthickness=0, command=start_timer)
 button_start.grid(column=0, row=2)
 
-button_reset = Button(text="Reset", highlightthickness=0)
+button_reset = Button(text="Reset", highlightthickness=0, command=reset_timer)
 button_reset.grid(column=2, row=2)
 
-label_check = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20, "bold"))
+label_check = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20, "bold"))
 label_check.grid(column=1, row=3)
 
 window.mainloop()
